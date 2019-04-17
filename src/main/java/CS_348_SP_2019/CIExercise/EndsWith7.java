d88ff6cbd95fb29c785803dc016bbd86169c6810
dekeh1 <dekeh1@worcester.edu>
2019-04-17 08:57:44 -0400

    Updated Version

--- src/main/java/CS_348_SP_2019/CIExercise/EndsWith7.java
+++ src/main/java/CS_348_SP_2019/CIExercise/EndsWith7.java
@@ -1,13 +1,13 @@
 package CS_348_SP_2019.CIExercise;
 public class EndsWith7 {
-	
+
 	String names;
 
 	public EndsWith7()  {
 		names = "";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
-
+		names+= "Dexter Ekeh\n";
 	}
 
 	@Override
@@ -16,4 +16,3 @@ public class EndsWith7 {
 	}
 
 }
-
